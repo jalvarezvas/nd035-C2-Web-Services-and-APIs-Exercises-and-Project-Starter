5174c009005a7c8639baaddf2813003924b0c11e
jalvarezvas <jacinto.alvarez.vas@gmail.com>
2021-02-04 21:43:09 +0100

    1 commit

---
+++ P02-VehiclesAPI/pricing-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO price (currency, price, vehicleId) VALUES ('USD', 5000, 1);
+INSERT INTO price (currency, price, vehicleId) VALUES ('USD', 6000, 2);
+INSERT INTO price (currency, price, vehicleId) VALUES ('USD', 7000, 3);
+INSERT INTO price (currency, price, vehicleId) VALUES ('USD', 8000, 4);
+INSERT INTO price (currency, price, vehicleId) VALUES ('USD', 9000, 5);
